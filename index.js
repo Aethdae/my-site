3e429b9c4d68a27aa1adb7acd3513a16a6d00505
Aethdae <Aethdae@gmail.com>
2025-12-18 01:41:30 -0600

    Added animated text and json support, as well as creating the basic datatypes for creating choices for the user

--- index.js
+++ index.js
@@ -2,15 +2,18 @@ const doc = document;
 const potentialItems = ["Pickaxe", "Torch", "Sword", "Axe", "Gun"];
 const introStrings = ["Greetings!", "Welcome to the journey.", "What is your name?"]
 var inventory = new Map();
-
-
+const jsonUrl = "./data.json"
+var jsonData;
+var playerName;
 
 /**
  * Launch to initialize state and animations
  * TODO: Add loading from cookies? JS? Json? LocalFile?
  */
-function startUp()
+async function startUp()
 {
+    await getJson();
+    
     let initCards = doc.getElementsByClassName("initCard");
     //html elements still don't have foreach
     //initCards.forEach((card) => animateCards(card));
@@ -24,6 +27,28 @@ function startUp()
     animateSideColumn("leftColumn");
     animateSideColumn("rightColumn");
     console.log("Columns are animating");
+    setupInventory();
+
+    displayBeginning();
+}
+
+/**
+ * Populates the data variable with global information to use.
+ */
+async function getJson()
+{
+    try {
+        const response = await fetch(jsonUrl);
+        if (!response.ok)
+        {
+            throw new Error(`Response: ${response.status}`);
+        }
+        const result = await response.json();
+        console.log(result);
+        jsonData = result;
+    } catch (error) {
+        console.error(error.message);
+    }
 }
 
 /**
@@ -45,17 +70,57 @@ function setupInventory()
 */
 function animateCardsLoad(id)
 {
+    let element = doc.getElementById(id);
     console.log(`card is animating!`);
-    doc.getElementById(id).style.transition = "transform ease-in-out 1.2s 0.0s";
-    doc.getElementById(id).style.transform = "scale(10000%)";
+    element.style.transition = "transform ease-in-out 1.2s 0.0s";
+    element.style.transform = "scale(10000%)";
 }
 
+/**
+ * Animates columns on the intro of the site.
+@param {string} id
+*/
 function animateSideColumn(id)
 {
-    doc.getElementById(id).style.height = "800px";
-    doc.getElementById(id).style.transition = "background-color ease-in-out 2.6s 0.0s";
-    doc.getElementById(id).style.backgroundColor = "#888888FF"
+    let element = doc.getElementById(id);
+
+    element.style.height = "800px";
+    element.style.transition = "background-color ease-in-out 2.6s 0.0s";
+    element.style.backgroundColor = "#888888FF"
+}
+
+/**
+ * Plays through intro, asks for name entry;
+*/
+function displayBeginning()
+{
+    let nameEntry = doc.getElementById("nameEntry");
+    setTimeout(() => {
+        animateText("titleCard", jsonData.pregameHeaderText[0])
+    }, 50);
+    setTimeout(() => {animateText( "titleCard", jsonData.pregameHeaderText[1])}, 4050);
+    setTimeout(() => {animateText( "titleCard", jsonData.pregameHeaderText[2])}, 8050);
+    //animateText("titleCard", jsonData.pregameHeaderText[0]);
+    nameEntry.hidden = false;
+}
+
+/**
+ * Removes the current text, then animates the string as typed out.
+ * @param {string} id ID of HTML element to animate text on.
+ * @param {string} string Text to animate
+ */
+function animateText(id, string)
+{
+    console.log("Text is animating.")
+    let element = document.getElementById(id);
+    element.innerHTML = "";
+    let splitString = string.split("");
+    //timer, loops over string adding one char at a time?
+    for(let x = 0; x < splitString.length * 50; x+=50)
+    {
+        setTimeout(() => {element.append(splitString[x/50])}, x);
+    }
 }
 
 startUp();
-setupInventory();
+
